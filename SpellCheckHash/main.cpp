66eb700dbfd387d47bcd38186f5d551dd2650958
Michael Gosling <contact@michaelgosling.me>
2019-04-10 12:16:34 -0300

    Create xcode project.

---
+++ SpellCheckHash/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpellCheckHash
+//
+//  Created by Michael Gosling on 2019-04-10.
+//  Copyright © 2019 Michael Gosling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
